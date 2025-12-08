75f3dd83ed67f54551fbd5ffb2cd0d563b0243da
DESKTOP-CA43GQ2\Emre <emreceyhnn@gmail.com>
2025-12-08 04:21:14 +0300

    Fix icon path in index.html and update API key in fetchApi.js

--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
+const API_KEY = "ed2682fe1cd0ecc2efbcdeb7464c5ffd";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_BASE_URL = "https://image.tmdb.org/t/p";
